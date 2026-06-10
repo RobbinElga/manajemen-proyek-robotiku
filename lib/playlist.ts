ee90b001fe1ebdad1eec75ad19ce4c59fad7cb4e
Elga <elgafirmantara27@gmail.com>
2026-06-10 22:03:56 +0700

    fix

--- lib/playlist.ts
+++ lib/playlist.ts
@@ -7,6 +7,6 @@ export interface Track {
 // Place the matching audio files in public/music/ to enable playback.
 export const PLAYLIST: Track[] = [
   { title: "I Miss You", artist: "From - Elga", src: "/music/imiss.mp3" },
-  { title: "Line Without You", artist: "From - Fuad", src: "/music/linewithout.mp3" },
+  { title: "Line Without Hook", artist: "From - Fuad", src: "/music/linewithout.mp3" },
   { title: "See You Again", artist: "From - Rohim", src: "/music/seeyou.mp3" },
 ];
